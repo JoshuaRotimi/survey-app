6e348871013299e064ddd1e67deba2c4de850c4c
Joshua <olorunnipajoshua@gmail.com>
2023-08-03 19:49:36 +0100

    update display

--- src/components/DisplayForms.jsx
+++ src/components/DisplayForms.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const DisplayForms = () => {
   const [storedData, setStoredData] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     const data = localStorage.getItem("questions");
@@ -13,28 +14,40 @@ const DisplayForms = () => {
 
   return (
     <div>
-      <h2>List of Saved Questionnaires</h2>
-      {Object.entries(storedData).map(([date, list], index) => {
-        return (
-          <div
-            key={index}
-            style={{
-              border: "1px solid black",
-              padding: "5px",
-              margin: "10px",
-            }}
-          >
-            <ul>
-              <Link to={`/questionnaires/${index}`}>
-                <li>
-                  <h3>Questionnaire {date}</h3>
-                  <p>This questionnaire has {list.questions[0].length} steps</p>
-                </li>
-              </Link>
-            </ul>
-          </div>
-        );
-      })}
+      {Object.values(storedData).length > 0 ? (
+        <>
+          <h2>List of Saved Questionnaires</h2>
+          {Object.entries(storedData).map(([date, list], index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  border: "1px solid black",
+                  padding: "5px",
+                  margin: "10px",
+                }}
+              >
+                <ul>
+                  <Link to={`/questionnaires/${index}`}>
+                    <li>
+                      <h3>Questionnaire {date}</h3>
+                      <p>
+                        This questionnaire has {list.questions[0].length} steps
+                      </p>
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+            );
+          })}
+          <button onClick={() => navigate("/")}>Go Back</button>
+        </>
+      ) : (
+        <>
+          <h2>You have not created any questionnaires</h2>
+          <button onClick={() => navigate("/")}>Create Questionnaire</button>
+        </>
+      )}
     </div>
   );
 };
